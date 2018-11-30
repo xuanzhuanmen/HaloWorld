3c5865f51ac1c316653eecb0df927890ba241d48
chengjaijun <825997676@qq.com>
2018-11-30 20:45:44 +0800

    build a project

---
+++ learningdemo/src/main/java/com/cheng/learningdemo/LearningdemoApplication.java
@@ -0,0 +1,12 @@
+package com.cheng.learningdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningdemoApplication.class, args);
+    }
+}
