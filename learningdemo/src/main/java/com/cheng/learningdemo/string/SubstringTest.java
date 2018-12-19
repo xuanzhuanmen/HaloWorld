7eeb3badc91dacec99c0af706cb51ffc8e01bbc1
chengjaijun <825997676@qq.com>
2018-12-19 21:32:18 +0800

    可能报StringIndexOutOfBounds情况测试

--- learningdemo/src/main/java/com/cheng/learningdemo/string/SubstringTest.java
+++ learningdemo/src/main/java/com/cheng/learningdemo/string/SubstringTest.java
@@ -3,7 +3,17 @@ package com.cheng.learningdemo.string;
 public class SubstringTest {
     public static void main(String[] args) {
         String month = "2018-12-11";
+        System.out.println(month.length());
         String result = month.substring(0, 7);
         System.out.println(result);
+
+        //
+        String month2 = "";
+        if(month2.length() > 7) {
+            System.out.println(month2.substring(0, 7));
+        }else {
+            System.out.println("-------");
+        }
+
     }
 }
