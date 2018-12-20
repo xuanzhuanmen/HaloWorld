7683b4ae7af48dd522ea91ad230eb809dc280ba1
chengjaijun <825997676@qq.com>
2018-12-20 12:21:20 +0800

    sprint boot+slf4j+logback first test

---
+++ learningdemo/src/main/java/com/cheng/learningdemo/log/SpringBootLogBackTest.java
@@ -0,0 +1,12 @@
+package com.cheng.learningdemo.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpringBootLogBackTest {
+    private final static Logger logger = LoggerFactory.getLogger(SpringBootLogBackTest.class);
+
+    public static void main(String[] args) {
+        logger.info("logback success!");
+    }
+}
