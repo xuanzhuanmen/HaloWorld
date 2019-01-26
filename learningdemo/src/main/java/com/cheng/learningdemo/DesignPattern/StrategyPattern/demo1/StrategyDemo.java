4137687f5e83b845cbbf1b03ed0d618a7d6fa906
chengHome <825997676@qq.com>
2019-01-26 17:33:00 +0800

    feat: 策略模式demo1——添加总结

--- learningdemo/src/main/java/com/cheng/learningdemo/DesignPattern/StrategyPattern/demo1/StrategyDemo.java
+++ learningdemo/src/main/java/com/cheng/learningdemo/DesignPattern/StrategyPattern/demo1/StrategyDemo.java
@@ -7,6 +7,9 @@ package com.cheng.learningdemo.DesignPattern.StrategyPattern.demo1;
  * 跟简单工厂模式相比较，好处在于，创建对象的时候，简单工厂需要知道CashSuper类和Factory类。
  * 而策略模式，我们只需要知道CashContext这一个类即可。耦合度更低了。
  * 用CashContext创建对象，然后调用CashContext的相应方法。这类封装是更彻底的。
+ * 总结：
+ * 策略模式封装了变化。
+ * 当我们在实际业务分析时候，发现很多种业务场景判断，需要在不同时间应用不同的业务规则。那么就可以考虑策略模式了。
  */
 public class StrategyDemo {
     public static void main(String[] args) {
